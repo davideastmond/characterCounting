932632b72981501ea546cd2988401efaae513eb3
David Eastmond <davebuck1981@gmail.com>
2019-05-01 00:05:30 -0400

    finished problem

---
+++ characterCounting.js
@@ -0,0 +1,20 @@
+let countCharacters = function(str) {
+  // Input a string and return an object with shows each character and the count
+  // As a key value pair
+
+  // First strip of spaces
+  str = str.split(" ").join('');
+
+  // Create an empty object to return
+  let retObj = {}; 
+
+  for (let i = 0; i < str.length; i++) {
+    if (!(retObj.hasOwnProperty(str[i]))) {
+      retObj[str[i]] = 1;
+    } else {
+      retObj[str[i]] += 1;
+    }
+  }
+  return retObj;
+}
+console.log(countCharacters("lighthouse \n in the house"));
